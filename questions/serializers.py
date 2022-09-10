b278fdbbfad24f09d8eceaf7763ce0fbf9cba0e8
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-10 19:44:05 +0000

    Added owner field in Question model and serializer

--- questions/serializers.py
+++ questions/serializers.py
@@ -3,9 +3,10 @@ from .models import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Question
         fields = [
-            'id', 'summary', 'question', 'created_at', 'updated_at'
+            'id', 'summary', 'question', 'owner', 'created_at', 'updated_at'
         ]
