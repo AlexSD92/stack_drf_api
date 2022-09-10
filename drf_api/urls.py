9e12206b8c1ccf8a1d861347e1caa0adddf7f82f
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-10 12:03:13 +0000

    Serializers.py file created to serializer the Profile model, ProfileSerializer; ProfileList view created inheriting from generics.ListCreateAPIView; Added path to project urls and app urls for the listview

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('questions/', include('questions.urls')),
     path('answers/', include('answers.urls')),
+    path('profiles/', include('profiles.urls')),
 
     path('api-auth/', include('rest_framework.urls')),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Profile
+from .serializers import ProfileSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ProfileList(generics.ListCreateAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
