690cdf1a4de0e3593464720265597bd5b49ed3b1
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-25 10:16:53 +0000

    CORS commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -38,10 +38,7 @@ CORS_ORIGIN_WHITELIST = (
        'http://localhost:3000',
        'https://stack-react.herokuapp.com', 
        'http://stack-react.herokuapp.com', 
-       'https://3000-alexsd92-stackreact-l043975xff1.ws-eu67.gitpod.io',
-       'https://3000-alexsd92-stackreact-l043975xff1.ws-eu68.gitpod.io',
-       'https://3000-alexsd92-stackreact-x34gort52r2.ws-eu67.gitpod.io',
-       'https://3000-alexsd92-stackreact-x34gort52r2.ws-eu68.gitpod.io',
+       'https://3000-alexsd92-stackreact-6fq7d3m4yhl.ws-eu68.gitpod.io',
 
 )
 
@@ -59,10 +56,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'https://stack-drf-api.herokuapp.com', 
     'https://8000-alexsd92-stackdrfapi-a398cuy78ue.ws-eu64.gitpod.io',
-    'https://3000-alexsd92-stackreact-l043975xff1.ws-eu67.gitpod.io',
-    'https://3000-alexsd92-stackreact-l043975xff1.ws-eu68.gitpod.io',
-    'https://3000-alexsd92-stackreact-x34gort52r2.ws-eu67.gitpod.io',
-    'https://3000-alexsd92-stackreact-x34gort52r2.ws-eu68.gitpod.io',
+    'https://3000-alexsd92-stackreact-6fq7d3m4yhl.ws-eu68.gitpod.io',
     ]
 
 REST_USE_JWT = True
